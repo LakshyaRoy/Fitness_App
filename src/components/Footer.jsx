95840bcecbf63b6e791fc2032449749199193b16
lakshya <lakshyaroy848@gmail.com>
2024-03-04 00:25:14 +0530

    components created !

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <Box mt="40px" bgcolor="#FFF3F4">
+      <Stack
+        gap="40px"
+        alignItems="start"
+        px="40px"
+        py="24px"
+        justifyContent={"center"}
+        flexDirection={{ xs: "column", lg: "row" }}
+      >
+        <img src={Logo} alt="logo" width="auto" height="40px" />
+        <Typography variant="h5" textAlign={"center"}>
+          Elevate Your Workout Experience with GYM-APP💪🏽 © 2024 GYM-APP. All
+          Rights Reserved 😁
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import HorizontalScrollBar from "./HorizontalScrollBar";
+import Loader from "./Loader";
 
-const SimilarExercises = () => {
-  return <div>SimilarExercises</div>;
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <Box sx={{ ml: { lg: "100px", xs: "20px" } }}>
+      <Typography variant="h3" mb={5}>
+        Execrises that target the same muscle group
+      </Typography>
+      <Stack
+        direction={"row"}
+        sx={{ p: "2", position: "relative" }}
+        gap={"40px"}
+        alignItems={"center"}
+        flexWrap={"wrap"}
+        justifyContent={"center"}
+      >
+        {targetMuscleExercises.length ? (
+          <HorizontalScrollBar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography variant="h3" mb={5}>
+        Execrises that use the same equipment
+      </Typography>
+      <Stack
+        direction={"row"}
+        sx={{ p: "2", position: "relative" }}
+        gap={"40px"}
+        alignItems={"center"}
+        flexWrap={"wrap"}
+        justifyContent={"center"}
+      >
+        {equipmentExercises?.length ? (
+          <HorizontalScrollBar data={equipmentExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+    </Box>
+  );
 };
 
 export default SimilarExercises;
